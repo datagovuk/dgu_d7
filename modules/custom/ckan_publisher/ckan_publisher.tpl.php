25477bc6b9243db49fa219de394cc5b7bb865b45
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2013-05-30 17:04:24 +0100

    Add optional handling CKAN UUID's in ckan_dataset and ckan_publisher entities callback

--- modules/custom/ckan_publisher/ckan_publisher.tpl.php
+++ modules/custom/ckan_publisher/ckan_publisher.tpl.php
@@ -29,11 +29,9 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>>
-        <a href="<?php print $url; ?>"><?php print $title; ?></a>
-    </h2>
-  <?php endif; ?>
+  <h2<?php print $title_attributes; ?>>
+      <a href="<?php print $url; ?>"><?php print $title; ?></a>
+  </h2>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php
