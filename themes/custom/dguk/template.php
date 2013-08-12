641fecf59a47856a59d77159580bc2b5310386aa
carinadigital <git@carinadigital.com>
2013-08-12 17:23:07 +0300

    Add the shared javascript file.

--- themes/custom/dguk/template.php
+++ themes/custom/dguk/template.php
@@ -10,4 +10,15 @@ function dguk_preprocess(&$variables){
     # Add the shared dgu logo.
     $variables['logo'] = $base_root."/assets/img/dgu-header-cropped.png";
 
+
+    # Add shared CSS and JS. Added here because the info file won't allow inclusion from site root.
+
+    
+    drupal_add_js('/assets/js/vendor.compiled.js', array(
+      'type' => 'file',
+      'scope' => 'header',
+      'group' => JS_THEME,
+      'every_page' => TRUE,
+    ));
+
 }
