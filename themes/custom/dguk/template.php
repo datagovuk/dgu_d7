9a378c4273df64736d035b4e7b9c17cd22b83163
carinadigital <git@carinadigital.com>
2013-08-12 16:19:36 +0300

    Change bootstrap's default logo to our shared one.

--- themes/custom/dguk/template.php
+++ themes/custom/dguk/template.php
@@ -3,6 +3,11 @@
 
 
 function dguk_preprocess(&$variables){
-	module_load_include('inc', 'lexicon', 'lexicon.pages');
-	//$variables['']
+    global $base_root;
+
+    module_load_include('inc', 'lexicon', 'lexicon.pages');
+
+    # Add the shared dgu logo.
+    $variables['logo'] = $base_root."/assets/img/dgu-header-cropped.png";
+
 }
