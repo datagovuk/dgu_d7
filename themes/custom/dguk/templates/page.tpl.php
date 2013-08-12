a5bb448fe1d8fab397c907bc25cf0252d302e928
Noel Sharpe <noels@radnetwork.co.uk>
2013-08-12 21:33:50 +0100

    Update page template

--- themes/custom/dguk/templates/page.tpl.php
+++ themes/custom/dguk/templates/page.tpl.php
@@ -1,3 +1,55 @@
+<div id="blackbar">
+    <div class="container">
+        <div id="hm-government-link" class="retina-img">
+            <img src="/assets/img/crown-and-text.png" alt="HM Government" />
+        </div>
+            <span class="ckan-logged-in" style="display: none;">
+                <div id="login-or-signup">
+                    You are logged-in as
+                    <a href="${'/user' if ('dgu_drupal_auth' in config['ckan.plugins']) else h.url_for(controller='user',action='me')}">${c.userobj.fullname if (c.userobj and c.userobj.fullname) else c.user}</a>.
+                    <a href="${'/logout' if ('dgu_drupal_auth' in config['ckan.plugins']) else h.url_for('/data/user/logout')}">Log out</a>.
+                </div>
+            </span>
+            <span class="ckan-logged-out">
+                <div id="login-or-signup">
+                    <a href="${'/user' if ('dgu_drupal_auth' in config['ckan.plugins']) else h.url_for(controller='user',action='login')}">Log in</a>
+                    or
+                    <a href="${'/user/register' if ('dgu_drupal_auth' in config['ckan.plugins']) else h.url_for(controller='user',action='register')}">sign up</a>
+                </div>
+            </span>
+    </div>
+</div>
+<div id="greenbar" class="">
+    <div class="container">
+        <a class="btn btn-inverse visible-phone" data-toggle="collapse" data-target=".main-nav-collapse">
+            Nav &nbsp;<i class="icon-chevron-down icon-white"></i>
+        </a>
+        <a class="brand" href="#" rel="home">
+            <div id="dgu-header" class="retina-img">
+                <img src="/assets/img/dgu-header-cropped.png" alt="DATA.GOV.UK - Opening up Government" />
+            </div>
+        </a>
+    </div>
+    <div class="container">
+        <div class="navbar navbar-inverse">
+            <div class="main-nav-collapse">
+                <ul id="dgu-nav" class="nav">
+                    <li class="nav-home active"><a href="/">Home</a></li>
+                    <li class="nav-data"><a href="/data">Data</a></li>
+                    <li class="nav-participate"><a href="/participate">Participate</a></li>
+                    <li class="nav-data-requests"><a href="/odug">Data Requests</a></li>
+                    <li class="nav-apps"><a href="/apps">Apps</a></li>
+                    <li class="nav-location"><a href="/location">Location</a></li>
+                    <li class="nav-linked-data"><a href="/linked-data">Linked Data</a></li>
+                    <li class="nav-library"><a href="/library">Library</a></li>
+                    <li class="nav-about"><a href="/about-us">About</a></li>
+                </ul>
+            </div><!--/.main-nav-collapse -->
+        </div>
+    </div>
+</div>
+
+
 <header id="navbar" role="banner" class="navbar navbar-fixed-top">
     <div class="navbar-inner">
         <div class="container">
