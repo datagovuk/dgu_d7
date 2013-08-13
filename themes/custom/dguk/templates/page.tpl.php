7bf0e1838971a1d841cac0f23664cea003783b7d
Noel Sharpe <noels@radnetwork.co.uk>
2013-08-13 15:50:10 +0100

    Revert html and page template markup to exactly what was in the dgu_theme repository

--- themes/custom/dguk/templates/page.tpl.php
+++ themes/custom/dguk/templates/page.tpl.php
@@ -50,98 +50,58 @@
     </div>
 </div>
 
+<div class="container content-container">
 
-<header id="navbar" role="banner" class="navbar navbar-fixed-top">
-    <div class="navbar-inner">
-        <div class="container">
-            <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
+    <div class="page">
+        <?php print $breadcrumb; ?>
 
-            <?php if ($logo): ?>
-                <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-                </a>
-            <?php endif; ?>
+        <?php if($page['highlighted'] OR $messages){ ?>
+            <div class="drupal-messages">
+                <?php print render($page['highlighted']); ?>
+                <?php print $messages; ?>
+            </div>
+        <?php } ?>
 
-            <?php if ($site_name): ?>
-                <h1 id="site-name">
-                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
-                </h1>
-            <?php endif; ?>
+        <div role="main" id="main-content">
 
-            <?php if ($primary_nav || $secondary_nav || !empty($page['navigation'])): ?>
-                <div class="nav-collapse">
-                    <nav role="navigation">
-                        <?php if ($primary_nav): ?>
-                            <?php print render($primary_nav); ?>
-                        <?php endif; ?>
-                        <?php if (!empty($page['navigation'])): ?>
-                            <?php print render($page['navigation']); ?>
-                        <?php endif; ?>
-                        <?php if ($secondary_nav): ?>
-                            <?php print render($secondary_nav); ?>
-                        <?php endif; ?>
-                    </nav>
-                </div>
-            <?php endif; ?>
-        </div>
-    </div>
-</header>
-
-<div class="container">
-
-    <header role="banner" id="page-header">
-        <?php if ( $site_slogan ): ?>
-            <p class="lead"><?php print $site_slogan; ?></p>
-        <?php endif; ?>
-
-        <?php print render($page['header']); ?>
-    </header> <!-- /#header -->
-
-    <div class="row">
-
-        <?php if ($page['sidebar_first']): ?>
-            <aside class="span3" role="complementary">
-                <?php print render($page['sidebar_first']); ?>
-            </aside>  <!-- /#sidebar-first -->
-        <?php endif; ?>
-
-        <section class="<?php print _bootstrap_content_span($columns); ?>">
-            <?php if ($page['highlighted']): ?>
-                <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-            <?php endif; ?>
-            <?php if ($breadcrumb): print $breadcrumb; endif;?>
-            <a id="main-content"></a>
             <?php print render($title_prefix); ?>
             <?php if ($title): ?>
-                <h1 class="page-header"><?php print $title; ?></h1>
+                <h1><?php print $title; ?></h1>
             <?php endif; ?>
             <?php print render($title_suffix); ?>
-            <?php print $messages; ?>
-            <?php if ($tabs): ?>
-                <?php print render($tabs); ?>
-            <?php endif; ?>
-            <?php if ($page['help']): ?>
-                <div class="well"><?php print render($page['help']); ?></div>
-            <?php endif; ?>
+
             <?php if ($action_links): ?>
                 <ul class="action-links"><?php print render($action_links); ?></ul>
             <?php endif; ?>
-            <?php print render($page['content']); ?>
-        </section>
 
-        <?php if ($page['sidebar_second']): ?>
-            <aside class="span3" role="complementary">
-                <?php print render($page['sidebar_second']); ?>
-            </aside>  <!-- /#sidebar-second -->
+            <?php if (isset($tabs['#primary'][0]) || isset($tabs['#secondary'][0])): ?>
+                <nav class="tabs"><?php print render($tabs); ?></nav>
+            <?php endif; ?>
+
+            <?php print render($page['content_pre']); ?>
+
+            <?php print render($page['content']); ?>
+
+            <?php print render($page['content_post']); ?>
+
+        </div><!--/main-->
+
+        <?php if ($page['sidebar_first']): ?>
+            <div class="sidebar-first" id="sidebar1">
+                <?php print render($page['sidebar_first']); ?>
+            </div>
         <?php endif; ?>
 
-    </div>
-    <footer class="footer container">
+        <?php if ($page['sidebar_second']): ?>
+            <div class="sidebar-second" id="sidebar2">
+                <?php print render($page['sidebar_second']); ?>
+            </div>
+        <?php endif; ?>
+        <div class="clearfix"></div>
+    </div><!--/page-->
+
+    <footer role="contentinfo">
         <?php print render($page['footer']); ?>
     </footer>
-</div>
+</div><!--/.content-container-->
+
