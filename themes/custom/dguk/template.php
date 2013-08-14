b52124c8f9df82ebddd0ac6eb18ebf23e1443b50
carinadigital <git@carinadigital.com>
2013-08-14 16:01:30 +0300

    Add function to print out the main menu with correct id and classes.

--- themes/custom/dguk/template.php
+++ themes/custom/dguk/template.php
@@ -10,3 +10,25 @@ function dguk_preprocess(&$variables){
     # Add the shared dgu logo.
     $variables['logo'] = '/assets/img/dgu-header-cropped.png';
 }
+
+
+/**
+ * Get the output for the main menu.
+ * Used in template and for ckan to pull in externally.
+ */
+function dguk_get_main_menu() {
+	$menu = menu_navigation_links('main-menu');
+	$menu_output = theme('links__main-menu', array(
+	    'links' => $menu,
+	    'attributes' => array(
+	        'id' => 'dgu-nav',
+	        'class' => array('nav'),
+	    ),
+	 ));
+
+    $output = '<div class="navbar navbar-inverse"> <div class="main-nav-collapse">';
+    $output .=  $menu_output;
+    $output .= '</div><!--/.main-nav-collapse --></div>';
+
+	return $output;
+ }
