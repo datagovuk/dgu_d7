23fd5baf71bb6090c95487e272bc3eccfd2688d5
carinadigital <git@carinadigital.com>
2013-08-19 11:58:19 +0300

    Override the comments template by adding achor. We use this to direct link to the comments.

--- themes/custom/dguk/templates/replies.tpl.php
+++ themes/custom/dguk/templates/replies.tpl.php
@@ -1,4 +1,5 @@
 <div class="replies-wrapper">
+  <a id="comments"></a> <!--Anchor to display nodes' comments.-->
   <?php if (!empty($header)) : ?><div class="replies-header"><h3><?php print $header ?></h3></div><?php endif; ?>
   <div class="replies"><?php print render($replies) ?></div>
   <?php if ($links): ?><div class="replies-links"><?php print render($links) ?></div><?php endif; ?>
