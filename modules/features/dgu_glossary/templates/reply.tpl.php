e7beedc766e8f7153c95545f66f1b2d420faed19
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2013-08-23 14:21:22 +0200

    Remove reply and user profile styling from glossary feature - these changes are site wide and will be put in the theme

--- modules/features/dgu_glossary/templates/reply.tpl.php
+++
@@ -1,7 +0,0 @@
-<div id="reply-<?php print $reply->id ?>" class="<?php print $classes ?>">
-    <?php
-    print $author;?>
-
-    <div class="reply-body"><?php print render($content) ?></div>
-  <div class="reply-links"><?php print render($links) ?></div>
-</div>
