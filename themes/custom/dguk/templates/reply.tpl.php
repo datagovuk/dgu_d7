46fac62e23892f7c8ff184564ab6b721f2e16f91
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2013-08-23 14:49:03 +0200

    Copy reply.tpl.php in to theme

---
+++ themes/custom/dguk/templates/reply.tpl.php
@@ -0,0 +1,4 @@
+<div id="reply-<?php print $reply->id ?>" class="<?php print $classes ?>">
+  <div class="reply-body"><?php print render($content) ?></div>
+  <div class="reply-links"><?php print render($links) ?></div>
+</div>
