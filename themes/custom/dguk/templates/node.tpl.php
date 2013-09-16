d806c01c723777a4c833b1327317443117362464
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2013-09-16 13:54:40 +0100

    Remove node--blog.tpl.php, remove $conent['comments'] from node tpl.php because we use Reply module instead of Comment.

---
+++ themes/custom/dguk/templates/node.tpl.php
@@ -0,0 +1,31 @@
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page && $title): ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($display_submitted): ?>
+      <span class="submitted">
+        <?php print $user_picture; ?>
+        <?php print $submitted; ?>
+      </span>
+    <?php endif; ?>
+  </header>
+
+  <?php
+    // Hide comments, tags, and links now so that we can render them later.
+    hide($content['links']);
+    print render($content);
+  ?>
+
+  <?php if (!empty($content['links'])): ?>
+    <footer>
+      <?php print render($content['links']); ?>
+    </footer>
+  <?php endif; ?>
+
+</article> <!-- /.node -->
