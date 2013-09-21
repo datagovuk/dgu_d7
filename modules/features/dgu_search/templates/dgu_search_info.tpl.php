8eeb87321260e49e49d608354558de1cf833773e
Noel Sharpe <noels@radnetwork.co.uk>
2013-09-21 15:02:49 +0100

    Add DGU search panel functionality which displays some information about the current solr search. This new panel will be available from the Panel's ui.

---
+++ modules/features/dgu_search/templates/dgu_search_info.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Variables $
+ */
+
+?>
+<div class="result-info">
+    <div class="result-count"><?php print $found ?></div>
+    <div class="result-count-footer"><?php print $type ?></div>
+</div>
