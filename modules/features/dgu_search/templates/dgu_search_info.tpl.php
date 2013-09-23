51a0edefadf0f76c9e536810683b89f7a92e3498
Noel Sharpe <noels@radnetwork.co.uk>
2013-09-23 12:05:50 +0100

    Add count of private data requests to solr result info panel.

--- modules/features/dgu_search/templates/dgu_search_info.tpl.php
+++ modules/features/dgu_search/templates/dgu_search_info.tpl.php
@@ -8,4 +8,7 @@
 <div class="result-info">
     <div class="result-count"><?php print $found ?></div>
     <div class="result-count-footer"><?php print $type ?></div>
+    <?php if (isset($dataset_request_count)): ?>
+    <div class="result-private-dataset-request">+ <?php print $dataset_request_count ?> private data requests</div>
+    <?php endif ?>
 </div>
