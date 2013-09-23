7bcc8e8d54fc516d79be5913a6d50af8fef0f3ea
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2013-09-23 15:31:27 +0100

    Issue #831 - Add author and created date to reply template

--- themes/custom/dguk/templates/reply.tpl.php
+++ themes/custom/dguk/templates/reply.tpl.php
@@ -1,4 +1,7 @@
 <div id="reply-<?php print $reply->id ?>" class="<?php print $classes ?>">
+  <?php hide($content['field_reply_subject']); ?>
+  <h3><?php print $reply->field_reply_subject[LANGUAGE_NONE][0]['safe_value']; ?></h3>
+  <div>Posted by <?php print $author; ?> on <?php print $created; ?></div>
   <div class="reply-body"><?php print render($content) ?></div>
   <div class="reply-links"><?php print render($links) ?></div>
 </div>
