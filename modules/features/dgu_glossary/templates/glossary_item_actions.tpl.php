b309578a13b2a6f0876e979b97d1ec77b41e2f6d
Noel Sharpe <noels@radnetwork.co.uk>
2013-11-17 19:31:53 +0000

    Wrap anchor around button for glossary item actions

--- modules/features/dgu_glossary/templates/glossary_item_actions.tpl.php
+++ modules/features/dgu_glossary/templates/glossary_item_actions.tpl.php
@@ -3,9 +3,11 @@
   <?php print t($suggest_text); ?>
 </button>
 </a>
+<a href="<?php print $comment_path ?>">
 <button type="button" class="btn btn-default">
-  <?php print l($comment_text, $comment_path, $comment_link_options); ?>
+  <?php print t($comment_text); ?>
 </button>
+</a>
 <p>
   You can help us refine and improve the definition of the term: Aggregated data.
   You can <?php print l($suggest_text, $suggest_path, $suggest_link_options); ?>
