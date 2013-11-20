7f65fcbc665c560734ef4b9179f0c001cfdb7e8f
Noel Sharpe <noels@radnetwork.co.uk>
2013-11-20 14:30:29 +0000

    Fix issues with incorrect icon showing

--- modules/features/dgu_glossary/widgets/endorse/widget.tpl.php
+++ modules/features/dgu_glossary/widgets/endorse/widget.tpl.php
@@ -9,23 +9,24 @@
 <div class="vud-widget vud-widget-endorse" id="<?php print $id; ?>">
 <?php if ($class_up) : ?>
   <?php if ($show_links): ?>
-    <?php if ($show_up_as_link): ?>
-      <a href="<?php print $link_up; ?>" rel="nofollow" class="<?php print $link_class_up; ?>">
+    <?php if (!$show_reset): ?>
+      <?php if ($show_up_as_link): ?>
+        <a href="<?php print $link_up; ?>" rel="nofollow" class="<?php print $link_class_up; ?>">
+      <?php endif; ?>
+      <button type="button" class="btn btn-default">
+        <i class="icon-thumbs-up"></i>
+        <?php print t('Endorse'); ?>
+      </button>
+      <?php if ($show_up_as_link): ?>
+        </a>
+      <?php endif; ?>
     <?php endif; ?>
-    <button type="button" class="btn btn-default">
-      <span class="glyphicon glyphicon-thumbs-up"></span>
-      <?php print t('Endorse'); ?>
-    </button>
-    <?php if ($show_up_as_link): ?>
-      </a>
-    <?php endif; ?>
-
     <?php if ($show_reset): ?>
         <a href="<?php print $link_reset; ?>" rel="nofollow" class="<?php print $link_class_reset; ?>"
            title="<?php print $reset_long_text; ?>">
           <button type="button" class="btn btn-default">
-            <span class=".glyphicon .glyphicon-ban-circle"></span>
-            <?php print $reset_short_text; ?>
+            <i class="icon-thumbs-down"></i>
+            <?php print t('Cancel Endorsement'); ?>
           </button>
         </a>
     <?php endif; ?>
