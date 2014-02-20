9f66f635234d475bca7a96ddf407d166bdde6e34
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-02-20 12:45:23 +0000

    Add "node-title" class to app node title

--- themes/custom/dguk/templates/node--app.tpl.php
+++ themes/custom/dguk/templates/node--app.tpl.php
@@ -8,7 +8,7 @@
         <h2 class="node-title" <?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
       <? endif; ?>
     <?php elseif ($title): ?>
-      <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+      <h2 class="node-title" <?php print $title_attributes; ?>><?php print $title; ?></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
     <?php if ($display_submitted): ?>
