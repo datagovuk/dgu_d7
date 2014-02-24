9c3d6203d63d3f620398a8ce105632b8e1732977
Noel Sharpe <noels@radnetwork.co.uk>
2014-02-24 14:10:02 +0000

    Add id to buttons to enable targeting with Tour possible.

--- modules/features/dgu_glossary/templates/glossary_item_actions.tpl.php
+++ modules/features/dgu_glossary/templates/glossary_item_actions.tpl.php
@@ -1,10 +1,10 @@
 <a href="<?php print $suggest_path; ?>">
-<button type="button" class="btn btn-default btn-primary">
+<button id="suggest-def" type="button" class="btn btn-default btn-primary">
   <?php print t($suggest_text); ?>
 </button>
 </a>
 <a href="<?php print $comment_path; ?>">
-<button type="button" class="btn btn-default btn-primary">
+<button id="make-comment" type="button" class="btn btn-default btn-primary">
   <?php print t($comment_text); ?>
 </button>
 </a>
