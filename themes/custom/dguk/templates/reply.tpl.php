0adef331bcf46e58d94ef609bfa70f2f8a20519d
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-03-26 17:14:11 +0000

    Styling changes in reply.tpl.php

--- themes/custom/dguk/templates/reply.tpl.php
+++ themes/custom/dguk/templates/reply.tpl.php
@@ -1,9 +1,13 @@
-<div id="reply-<?php print $reply->id ?>" class="<?php print $classes ?> boxed">
-  <?php print $avatar; ?>
-  <?php hide($content['field_reply_subject']); ?>
-  <h3><?php print $reply->field_reply_subject[LANGUAGE_NONE][0]['safe_value']; ?></h3>
-  <span class="submitted">Posted by <?php print $author; ?> on <?php print $created; ?></span>
-  <div class="reply-body"><?php print render($content) ?></div>
-  <?php print flag_create_link('spam_reply', $reply->id); ?>
-  <div class="reply-links"><?php print render($links) ?></div>
+<div id="reply-<?php print $reply->id; ?>" class="<?php print $classes ?> boxed">
+  <div class="inner">
+    <div class="reply-header">
+      <?php print $avatar; ?>
+      <?php hide($content['field_reply_subject']); ?>
+      <h3><?php print $reply->field_reply_subject[LANGUAGE_NONE][0]['safe_value']; ?></h3>
+      <span class="submitted">Posted by <?php print $author; ?> on <?php print $created; ?></span>
+    </div>
+    <div class="reply-body"><?php print render($content) ?></div>
+    <?php print flag_create_link('spam_reply', $reply->id); ?>
+    <div class="reply-links pull-left"><?php print render($links) ?></div>
+  </div>
 </div>
