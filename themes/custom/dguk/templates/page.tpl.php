3085b57c2749cf0e1c5107cff0acc78b10438293
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-03-26 16:41:48 +0000

    Don't render page title in page.tpl.php

--- themes/custom/dguk/templates/page.tpl.php
+++ themes/custom/dguk/templates/page.tpl.php
@@ -115,11 +115,6 @@
           <div id="messages" ><?php print $messages; ?></div>
         <?php endif; ?>
     </div>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
 
     <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
