6e42cd26a1e033b36059587d7007b41310eeb052
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-03-26 17:31:23 +0000

    Avoid bug related to '.' character in forum category name

--- themes/custom/dguk/templates/views/views-view-unformatted--forum-categories--block-1.tpl.php
+++ themes/custom/dguk/templates/views/views-view-unformatted--forum-categories--block-1.tpl.php
@@ -10,10 +10,10 @@
 list($active_category) = array_slice(explode('/', $_SERVER['REQUEST_URI']), 2, 1);
 ?>
 <?php if (!empty($title)): ?>
-  <h3><?php print $title; ?></h3>
+  <h2><?php print $title; ?></h2>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <?php if (strpos($row, $active_category) > 0) { $classes_array[$id] .= ' active'; } ?>
+  <?php if (strpos(str_replace('.', '', $row), $active_category) > 0 || strpos($row, $active_category) > 0) { $classes_array[$id] .= ' active'; } // Check for an argument wih and without '.' to avoid problems with 'police.uk-data' in the url.?>
   <div <?php if ($classes_array[$id]) { print 'class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
   </div>
