8a9f47c9181e8bc3e464a04befc8a02f3c9bcc3b
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-10 11:37:43 +0100

    Remove buttons from inside the links

--- modules/features/dgu_glossary/widgets/endorse/widget.tpl.php
+++ modules/features/dgu_glossary/widgets/endorse/widget.tpl.php
@@ -10,20 +10,16 @@
 <?php if ($class_up) : ?>
   <?php if ($show_links): ?>
     <?php if ($show_reset): ?>
-        <a href="<?php print $link_reset; ?>" rel="nofollow" class="<?php print $link_class_reset; ?>"
+        <a class="btn btn-default btn-primary" href="<?php print $link_reset; ?>" rel="nofollow" class="<?php print $link_class_reset; ?>"
            title="<?php print $reset_long_text; ?>">
-          <button type="button" class="btn btn-default btn-primary">
-            <i class="icon-thumbs-down"></i>
-            <?php print t('Cancel Endorsement'); ?>
-            <?php if (user_access('moderate glossary')) print "($points)" ?>
-          </button>
+          <i class="icon-thumbs-down"></i>
+          <?php print t('Cancel Endorsement'); ?>
+          <?php if (user_access('moderate glossary')) print "($points)" ?>
         </a>
     <?php elseif ($show_up_as_link): ?>
-        <a href="<?php print $link_up; ?>" rel="nofollow" class="<?php print $link_class_up; ?>">
-        <button type="button" class="btn btn-default btn-primary">
+        <a class="btn btn-default btn-primary" href="<?php print $link_up; ?>" rel="nofollow" class="<?php print $link_class_up; ?>">
           <i class="icon-thumbs-up"></i>
           <?php print t('Endorse'); ?>
-        </button>
         </a>
     <?php endif; ?>
   <?php endif; ?>
