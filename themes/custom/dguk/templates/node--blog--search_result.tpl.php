02ea31faa25fa5bc464bf9db1f621ffdc77ca1b8
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-28 13:45:29 +0100

    Add node--blog--search_result.tpl.php template

---
+++ themes/custom/dguk/templates/node--blog--search_result.tpl.php
@@ -0,0 +1,24 @@
+<div class="search-result-body">
+
+  <div class="right">
+  </div>
+
+  <div class="left">
+
+    <div class="search-info">
+      <?php if (isset($submitted)): ?>
+        <span><?php print $submitted; ?></span>
+      <?php endif; ?>
+
+      <?php if (isset($updated)): ?>
+        <span><?php print $updated; ?></span>
+      <?php endif; ?>
+    </div>
+    <?php
+    hide($content['links']);
+    hide($content['field_comment']);
+    print render($content);
+    ?>
+
+  </div>
+</div>
