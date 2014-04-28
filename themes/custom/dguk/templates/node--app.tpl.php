ad23ae236bb86551c338e187bcc83e008e1ac169
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-28 13:43:58 +0100

    Remove search_result related code from node-app.tpl.php

--- themes/custom/dguk/templates/node--app.tpl.php
+++ themes/custom/dguk/templates/node--app.tpl.php
@@ -2,11 +2,7 @@
   <header>
     <?php print render($title_prefix); ?>
     <?php if (!$page && $title): ?>
-      <?php if ($view_mode == "search_result"): ?>
-        <h3 class="node-title" <?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
-      <? else: ?>
-        <h1 class="node-title" <?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
-      <? endif; ?>
+      <h1 class="node-title" <?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
     <?php elseif ($title): ?>
       <h1 class="node-title" <?php print $title_attributes; ?>><?php print $title; ?></h1>
     <?php endif; ?>
@@ -23,19 +19,18 @@
       <?php endif; ?>
     <?php endif; ?>
   </header>
-
+  <?php print theme_fivestar_static(array('rating' => $field_rating[0]['average'], 'stars' => 5)); ?>
   <?php
     // Hide comments, tags, and links now so that we can render them later.
     hide($content['links']);
     hide($content['field_comment']);
     print render($content);
   ?>
-  <?php if ($view_mode != "search_result"): ?>
+
   <div class="disclaimer disclaimer-app">
     Apps submitted to data.gov.uk are currently approved for publication on the general level of their context and appropriateness.
     Whilst we review these on a periodical basis, we do not own responsibility for the regular update and maintenance of these apps. Any queries about individual apps or tools published need to be directed to the originator.
   </div>
-  <?php endif; ?>
 </article> <!-- /.node -->
 
 <?php if (!empty($content['links']) || !empty($content['field_comment'])): ?>
