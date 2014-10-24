da3316cd490ae86c5222b494ecc9e8b5a9cd41a3
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-10-24 12:16:31 +0100

    Indicate ckan admins in Publisher assignee field, attach CSS and JS files to data request edit form

---
+++ modules/features/dgu_data_set_request_publisher_assignee/js/publisher_assignee.js
@@ -0,0 +1,33 @@
+(function($) {
+
+    Drupal.behaviors.publisherChange = {
+        attach: function (context, settings) {
+            var PublisherY = $('#edit-field-publisher-ref input:checked').offset().top;
+            var PublisherWrapperY = $('#edit-field-publisher-ref').offset().top;
+            var offset = PublisherY - PublisherWrapperY - 3;
+            $('#edit-field-publisher-assignee').css({ 'padding-top': offset });
+
+            $('#edit-field-publisher-assignee label').append('<div class="publisher-assignee-connector"></div>');
+
+            $('#edit-field-publisher-ref li').css({ 'background': 'none'});
+            $('#edit-field-publisher-ref input:checked').closest('li').css({ 'background': '#f0fcff'});
+        }
+    }
+
+    Drupal.behaviors.dataRequestSubmit = {
+        attach: function (context, settings) {
+            $('input.form-submit').unbind("click").click(function(e) {
+                var publisher = $('#edit-field-publisher-ref input.form-radio:checked').val();
+                var publisherAssignee = $('#edit-field-publisher-assignee input.form-radio:checked').val();
+                if($.isNumeric(publisher) && !$.isNumeric(publisherAssignee)) {
+                    if (confirm("Publisher is set but Publisher assignee isn't.\nIn order to notify data holder about this request you must select user in Publisher assignee field.\n\nAre you sure that you want to save this request without notifying data holder?") != true) {
+                        e.preventDefault();
+                    }
+
+                }
+            });
+        }
+    }
+
+
+})(jQuery);
