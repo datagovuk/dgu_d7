657250254c65beb6b09abc96c905b90646e82d39
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2015-01-28 12:22:36 +0000

    Fix 'unable to get property' error

--- modules/features/dgu_data_set_request_publisher_assignee/js/publisher_assignee.js
+++ modules/features/dgu_data_set_request_publisher_assignee/js/publisher_assignee.js
@@ -2,18 +2,20 @@
 
     Drupal.behaviors.publisherChange = {
         attach: function (context, settings) {
-            var PublisherY = $('#edit-field-publisher-ref input:checked').offset().top;
-            var PublisherWrapperY = $('#edit-field-publisher-ref').offset().top;
-            var offset = PublisherY - PublisherWrapperY - 3;
-            $('#edit-field-publisher-assignee').css({ 'padding-top': offset });
+            if ($('#edit-field-publisher-ref input:checked').length > 0) {
+                var PublisherY = $('#edit-field-publisher-ref input:checked').offset().top;
+                var PublisherWrapperY = $('#edit-field-publisher-ref').offset().top;
+                var offset = PublisherY - PublisherWrapperY - 3;
+                $('#edit-field-publisher-assignee').css({ 'padding-top': offset });
 
-            $('#edit-field-publisher-assignee label').append('<div class="publisher-assignee-connector"></div>');
+                $('#edit-field-publisher-assignee label').append('<div class="publisher-assignee-connector"></div>');
 
-            $('#edit-field-publisher-ref li').css({ 'background': 'none'});
-            $('#edit-field-publisher-ref input:checked').closest('li').css({ 'background': '#f0fcff'});
+                $('#edit-field-publisher-ref li').css({ 'background': 'none'});
+                $('#edit-field-publisher-ref input:checked').closest('li').css({ 'background': '#f0fcff'});
 
-            if (typeof $('#edit-field-publisher-assignee input:checked').val() == 'undefined') {
-              $('#edit-field-publisher-assignee input.form-radio:first').attr('checked','checked');
+                if (typeof $('#edit-field-publisher-assignee input:checked').val() == 'undefined') {
+                    $('#edit-field-publisher-assignee input.form-radio:first').attr('checked','checked');
+                }
             }
         }
     }
