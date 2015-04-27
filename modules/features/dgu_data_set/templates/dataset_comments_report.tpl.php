62816172049b493266610a2d653b2d9cd2cbe7bf
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2015-04-27 11:02:21 +0100

    Show all comments on dataset comments report

--- modules/features/dgu_data_set/templates/dataset_comments_report.tpl.php
+++ modules/features/dgu_data_set/templates/dataset_comments_report.tpl.php
@@ -1,24 +1,12 @@
 <h1>Dataset comments</h1>
 <h3>Latest comments on all datasets published by <a href="/publisher/<?php print $publisher->name ?>"><?php print $publisher->title; ?></a></h3>
+<?php if($uid > 0): ?>
 <h4>Email notifications for dataset comments are <strong><?php print $email_notifications;?></strong>. You can change this preference in your <a href="/user/<?php print $uid; ?>/edit?destination=<?php $destination = drupal_get_destination(); print $destination['destination']; ?>">account settings</a></h4>
-<br/>
+<?php endif; ?>
 <div class="dataset-comment-report">
 <?php
-foreach  ($dataset_comments as $row):?>
-  <h3>Dataset: <a href="/dataset/<?php print $row->name; ?>"><?php print $row->title; ?></a></h3>
-  <div class="result boxed">
-    <div class="comment">
-      <div class="reply-header">
-        <h3><?php print $row->subject; ?></h3>
-        <?php if ($row->uid): ?>
-          <span class=""submitted">Posted by <a href="/user/<?php print $row->uid;?>"><?php print $row->user_name;?></a></span>
-        <?php else: ?>
-          <span class=""submitted">Posted by Visitor</span>
-        <?php endif; ?>
-        <span> on <?php print format_date($row->created) ;?> </span>
-      </div>
-      <p><?php print $row->comment;?></p>
-    </div>
-  </div>
+foreach  ($datasets_comments as $dataset):?>
+  <h3 class="dataset-title">Dataset: <a href="/dataset/<?php print $dataset['name']; ?>"><?php print $dataset['title']; ?></a></h3>
+    <?php print $dataset['comments']; ?>
 <?php endforeach ?>
 </div>
