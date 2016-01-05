366c14d474768fb8f2a735c308617dcda89f0e62
pawel <pawel@lampdevelopment.co.uk>
2016-01-05 19:22:51 +0000

    Add step to get response from /dataset/comment/get

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -652,7 +652,7 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext
           }
         }
       }
-      sleep(3);
+      sleep(5);
     }
     imap_close($mbox);
     // Throw Exception if message not found.
@@ -1296,6 +1296,30 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext
 
         $ckan_id = $dataset['result']['id'];
         $this->getSession()->visit($this->locatePath('/comment/dataset/' . $ckan_id));
+
+      } else {
+        throw new \Exception("Dataset '$dataset_name' not found.");
+      }
+
+    }
+    catch (Exception $e) {
+      throw new \Exception('CKAN client failed. ' . $e->getMessage());
+    }
+  }
+
+  /**
+   * @Given /^I get comments of dataset named "([^"]*)"$/
+   */
+  public function iGetCommentsOfDatasetNamed($dataset_name) {
+    try {
+      $client = $this->ckan_get_client();
+
+      $dataset = $client->GetDataset(array('id' => $dataset_name))->toArray();
+
+      if (!empty($dataset['result'])) {
+
+        $ckan_id = $dataset['result']['id'];
+        $this->getSession()->visit($this->locatePath('/comment/get/' . $ckan_id));
 
       } else {
         throw new \Exception("Dataset '$dataset_name' not found.");
