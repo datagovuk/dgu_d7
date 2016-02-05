504dfdd1967bb1086f7ff47247d3f39fac7745b4
Krzysztof Kraszewski <semiend@gmail.com>
2016-02-05 16:59:43 +0000

    issue #1963 remove ‘Email notifications for dataset comments are disabled’ message

--- modules/features/dgu_data_set/templates/dataset_comments_report.tpl.php
+++ modules/features/dgu_data_set/templates/dataset_comments_report.tpl.php
@@ -1,8 +1,5 @@
 <h1>Dataset comments</h1>
 <h3>Latest comments on all datasets published by <a href="/publisher/<?php print $publisher->name ?>"><?php print $publisher->title; ?></a></h3>
-<?php if($is_admin): ?>
-<h4>Email notifications for dataset comments are <strong><?php print $email_notifications;?></strong>. You can change this preference in your <a href="/user/<?php print $uid; ?>/edit?destination=<?php $destination = drupal_get_destination(); print $destination['destination']; ?>">account settings</a></h4>
-<?php endif; ?>
 <div class="dataset-comment-report">
 <?php
 foreach  ($datasets_comments as $dataset):?>
