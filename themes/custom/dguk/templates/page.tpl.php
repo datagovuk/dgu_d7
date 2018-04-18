468714a3f5edfee6f05abe4ed215b889a2e71164
Jonathan Nichols <nicholsj@users.noreply.github.com>
2018-04-18 09:51:59 +0100

    Remove survey message and link
    We no longer want the feedback survey on legacy pages.

--- themes/custom/dguk/templates/page.tpl.php
+++ themes/custom/dguk/templates/page.tpl.php
@@ -5,10 +5,6 @@ if (in_array('data publisher', array_values($user->roles))) {
 }
 ?>
 
-<div style="background-color: #128400; color: white; text-align: center; font-size: 1.3em; font-weight: 600; padding-top: 4px; padding-bottom: 4px;">
-<a href="http://www.smartsurvey.co.uk/s/Z06XA/" target="_blank" style="color:white; text-decoration: underline;">Complete a short survey</a> to share your feedback and tell us if you'd like to help us to test the new data.gov.uk site.
-</div>
-
 <?php $needs_admin_toolbar = ($user->uid == 1 || array_intersect(array('administrator', 'editor', 'blogger', 'data request administrator', 'moderator', 'CKAN sysadmin'), array_values($user->roles))) ?>
 <?php if(!$user->uid): ?>
   <?php $destination = drupal_get_destination(); ?>
